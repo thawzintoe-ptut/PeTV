7deae8e80fef8669f2d61f83f27f6b7f6507ae5a
ThawZinToe <developer.ptut@gmail.com>
2024-04-21 10:47:35 +0700

    initial commit

---
+++ app/src/main/java/com/ptut/pmovie/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.ptut.pmovie
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ptut.pmovie.ui.theme.PMovieTheme
+
+class MainActivity : ComponentActivity()
+{
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		enableEdgeToEdge()
+		setContent {
+			PMovieTheme {
+				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+					Greeting(
+						name = "Android", modifier = Modifier.padding(innerPadding)
+					)
+				}
+			}
+		}
+	}
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier)
+{
+	Text(
+		text = "Hello $name!", modifier = modifier
+	)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview()
+{
+	PMovieTheme {
+		Greeting("Android")
+	}
+}
